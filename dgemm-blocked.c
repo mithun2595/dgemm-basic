baaa3cd8068a63d7447aa18f7945f99c3b5974ac
dewalg <dewalgupta@gmail.com>
2017-10-14 02:49:21 -0700

    working on alignment

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -6,12 +6,13 @@
  *    Support CBLAS interface
  */
 #include <emmintrin.h>
+#include <stdlib.h>
 
 const char* dgemm_desc = "Simple blocked dgemm.";
 
 #if !defined(BLOCK_SIZE)
-#define BLOCK_2_SIZE 128
-#define BLOCK_1_SIZE 16
+#define BLOCK_2_SIZE 200
+#define BLOCK_1_SIZE 36
 #endif
 
 #define min(a,b) (((a)<(b))?(a):(b))
@@ -33,38 +34,115 @@ static void do_transpose(double* B, int lda, int K, int N, double* T) {
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
 static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
 {
-	double* T = malloc(K*N*sizeof(double));
+	//double* T = malloc(K*N*sizeof(double));
+	double* T = (double *) memalign(sizeof(double)*2, K*N*sizeof(double));
 	do_transpose(B, lda, K, N, T);
-	double cij = 0;
-	double* s = malloc(2*sizeof(double)); 
-	
-	for(int i=0;i<M;++i) {
+	double ci1j1 = 0; 
+	double ci2j1 = 0;
+	double ci1j2 = 0;
+	double ci2j2 = 0;
 
-		for(int j=0;j<N;++j) {
+	double* s_i1j1 = malloc(2*sizeof(double)); 
+	double* s_i2j1 = malloc(2*sizeof(double)); 
+	double* s_i1j2 = malloc(2*sizeof(double)); 
+	double* s_i2j2 = malloc(2*sizeof(double)); 
+
+	for(int i=0;i<M;i+=2) {
+
+		for(int j=0;j<N;j+=2) {
 			int K_iter = K;
-			cij = C[i*lda+j];
+
+			ci1j1 = C[i*lda+j];
+			if (j+1 >= N && i+1 >= M) {
+				ci2j2 = 0;
+				ci1j2 = 0;
+				ci2j1 = 0;
+			} else if (j+1 >= N) {
+				ci1j2 = 0;
+				ci2j2 = 0;
+				ci2j1 = C[(i+1)*lda+j];
+			} else if (i+1 >= M) {
+				ci2j1 = 0;
+				ci2j2 = 0;
+				ci1j2 = C[i*lda+j+1];
+			} else {
+				ci1j2 = C[i*lda+j+1];
+				ci2j1 = C[(i+1)*lda+j];
+				ci2j2 = C[(i+1)*lda+j+1];
+			}	
+
 			if (K%2 == 1) {
-				cij += A[i*lda+K-1] * T[K*j + K-1];			
+				ci1j1 += A[i*lda+K-1] * T[K*j + K-1];
+				
+				if (j+1 >= N && i+1 >= M) {
+					//do nothing	
+				} else if (i+1 >= M) {
+					ci1j2 += A[i*lda+K-1] * T[K*(j+1) + K-1];
+				} else if (j+1 >= N) {
+					ci2j1 += A[(i+1)*lda+K-1] * T[K*j+K-1];
+				} else {
+					ci1j2 += A[i*lda+K-1] * T[K*(j+1) + K-1];
+					ci2j1 += A[(i+1)*lda+K-1] * T[K*j+K-1];
+					ci2j2 += A[(i+1)*lda+K-1] * T[K*(j+1)+K-1];
+				}			
 				K_iter = K-1;
 			}
-			register __m128d c = _mm_setzero_pd();
-			register __m128d sum = _mm_setzero_pd();
-			register __m128d a;
-			register __m128d b;
+
+			register __m128d sum_1 = _mm_setzero_pd();
+			register __m128d sum_2 = _mm_setzero_pd();
+			register __m128d sum_3 = _mm_setzero_pd();
+			register __m128d sum_4 = _mm_setzero_pd();
+			register __m128d a_1, a_2;
+			register __m128d b_1, b_2;
 			for(int k=0;k<K_iter;k += 2) {
-				a = _mm_loadu_pd(&A[i*lda+k]);
-				b = _mm_loadu_pd(&T[K*j+k]);
-				c = _mm_mul_pd(a,b);
-				sum = _mm_add_pd(sum, c);
+				a_1 = _mm_loadu_pd(&A[i*lda+k]);
+				b_1 = _mm_load_pd(&T[K*j+k]);
+				
+				if (i+1 >= M && j+1 >= N) {
+					a_2 = _mm_setzero_pd();
+					b_2 = _mm_setzero_pd();
+				} else if (i+1 >= M) {
+					b_2 = _mm_load_pd(&T[K*(j+1)+k]);
+					a_2 = _mm_setzero_pd();
+				} else if (j+1 >= N) {
+					a_2 = _mm_loadu_pd(&A[(i+1)*lda+k]);
+					b_2 = _mm_setzero_pd();
+				} else {
+					a_2 = _mm_loadu_pd(&A[(i+1)*lda+k]);
+					b_2 = _mm_load_pd(&T[K*(j+1)+k]);
+				}
+				sum_1 = _mm_add_pd(sum_1, _mm_mul_pd(a_1,b_1));
+				sum_2 = _mm_add_pd(sum_2, _mm_mul_pd(a_1,b_2));
+				sum_3 = _mm_add_pd(sum_3, _mm_mul_pd(a_2,b_1));
+				sum_4 = _mm_add_pd(sum_4, _mm_mul_pd(a_2,b_2));
 				//cij += A[i*lda + k] * T[K*j + k];
 			}
-			_mm_storeu_pd(s, sum);
-			C[i*lda+j] = cij + s[0] + s[1];
+
+			_mm_storeu_pd(s_i1j1, sum_1);
+			_mm_storeu_pd(s_i1j2, sum_2);
+			_mm_storeu_pd(s_i2j1, sum_3);
+			_mm_storeu_pd(s_i2j2, sum_4);
 			
+			C[i*lda+j] = ci1j1 + s_i1j1[0] + s_i1j1[1];
+			if (j+1 >= N && i+1 >= M) {
+				//do nothing
+			} else if (i+1 >= M) {
+				C[i*lda+j+1] = ci1j2 + s_i1j2[0] + s_i1j2[1];
+			} else if (j+1 >= N) {
+				C[(i+1)*lda+j] = ci2j1 + s_i2j1[0] + s_i2j1[1];	
+			} else {
+				C[i*lda+j+1] = ci1j2 + s_i1j2[0] + s_i1j2[1];
+				C[(i+1)*lda+j] = ci2j1 + s_i2j1[0] + s_i2j1[1];
+				C[(i+1)*lda+j+1] = ci2j2 + s_i2j2[0] + s_i2j2[1];
+			}	
+
 		//	C[i*lda+j] = cij;
 		}
 	}
-	free(s);
+	free(s_i1j1);
+	free(s_i1j2);
+	free(s_i2j1);
+	free(s_i2j2);
 	free(T);
 }
 
