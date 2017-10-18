c4627a1f252a4cabc9957a02c018b58bc9023939
dewalg <dewalgupta@gmail.com>
2017-10-17 17:36:20 -0700

    implemented alignment - fixed mem issues

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -18,13 +18,14 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 #define min(a,b) (((a)<(b))?(a):(b))
 
 static void do_transpose(double* B, int lda, int K, int N, double* T) {
+	int K_ind = (K%2==1) ? K+1 : K;
 	for (int i=0; i < K; i++) {
 		for (int j= 0; j < N; j++) {
 		/*	double t = B[i*lda+j];
 			B[i*lda+j] = B[j*lda+i];
 			B[j*lda+i] = t;
 		*/
-			T[j*K+i] = B[i*lda+j];
+			T[j*K_ind+i] = B[i*lda+j];
 		}	
 	}
 }
@@ -32,20 +33,37 @@ static void do_transpose(double* B, int lda, int K, int N, double* T) {
 /* This auxiliary subroutine performs a smaller dgemm operation
  *  C := C + A * B
  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
-static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C)
+static void do_block (int lda, int M, int N, int K, double* A, double* B, double* C, double* T)
 {
-	//double* T = malloc(K*N*sizeof(double));
-	double* T = (double *) memalign(sizeof(double)*2, K*N*sizeof(double));
-	do_transpose(B, lda, K, N, T);
+	//K_ind  is the real unpadded index to which the matrix T goes to
+	//K      is the padded length of matrix (used to skip to column)
+	//K_iter is the iteration we go to for the multiplication
+	//
+	//if K_ind = 5, then K = 6 and K_iter would be 4
+	//for the special case of k=5, we handle it separately
+	//in the if-else statement below. This is so we can go through
+	//the matrix with a max k value of 4 (hence k_iter). 
+	 
+	int K_ind = K;
+	if (K%2==1) {
+		K = K+1;
+	}
+	
+	do_transpose(B, lda, K_ind, N, T);
 	double ci1j1 = 0; 
 	double ci2j1 = 0;
 	double ci1j2 = 0;
 	double ci2j2 = 0;
+  
+	double* s_i2j1 = NULL;
+	double* s_i1j2 = NULL;
+	double* s_i1j1 = NULL;
+	double* s_i2j2 = NULL;
 
-	double* s_i1j1 = malloc(2*sizeof(double)); 
-	double* s_i2j1 = malloc(2*sizeof(double)); 
-	double* s_i1j2 = malloc(2*sizeof(double)); 
-	double* s_i2j2 = malloc(2*sizeof(double)); 
+	posix_memalign(&s_i1j1, 16, 2*sizeof(double));
+	posix_memalign(&s_i2j1, 16, 2*sizeof(double));
+	posix_memalign(&s_i1j2, 16, 2*sizeof(double));
+	posix_memalign(&s_i2j2, 16, 2*sizeof(double));
 
 	for(int i=0;i<M;i+=2) {
 
@@ -71,21 +89,21 @@ static void do_block (int lda, int M, int N, int K, double* A, double* B, double
 				ci2j2 = C[(i+1)*lda+j+1];
 			}	
 
-			if (K%2 == 1) {
-				ci1j1 += A[i*lda+K-1] * T[K*j + K-1];
+			if (K_ind%2 == 1) {
+				ci1j1 += A[i*lda+K_ind-1] * T[K*j + K_ind-1];
 				
 				if (j+1 >= N && i+1 >= M) {
 					//do nothing	
 				} else if (i+1 >= M) {
-					ci1j2 += A[i*lda+K-1] * T[K*(j+1) + K-1];
+					ci1j2 += A[i*lda+K_ind-1] * T[K*(j+1) + K_ind-1];
 				} else if (j+1 >= N) {
-					ci2j1 += A[(i+1)*lda+K-1] * T[K*j+K-1];
+					ci2j1 += A[(i+1)*lda+K_ind-1] * T[K*j+K_ind-1];
 				} else {
-					ci1j2 += A[i*lda+K-1] * T[K*(j+1) + K-1];
-					ci2j1 += A[(i+1)*lda+K-1] * T[K*j+K-1];
-					ci2j2 += A[(i+1)*lda+K-1] * T[K*(j+1)+K-1];
+					ci1j2 += A[i*lda+K_ind-1] * T[K*(j+1) + K_ind-1];
+					ci2j1 += A[(i+1)*lda+K_ind-1] * T[K*j+K_ind-1];
+					ci2j2 += A[(i+1)*lda+K_ind-1] * T[K*(j+1)+K_ind-1];
 				}			
-				K_iter = K-1;
+				K_iter = K_ind-1;
 			}
 
 			register __m128d sum_1 = _mm_setzero_pd();
@@ -117,11 +135,11 @@ static void do_block (int lda, int M, int N, int K, double* A, double* B, double
 				sum_4 = _mm_add_pd(sum_4, _mm_mul_pd(a_2,b_2));
 				//cij += A[i*lda + k] * T[K*j + k];
 			}
-
-			_mm_storeu_pd(s_i1j1, sum_1);
-			_mm_storeu_pd(s_i1j2, sum_2);
-			_mm_storeu_pd(s_i2j1, sum_3);
-			_mm_storeu_pd(s_i2j2, sum_4);
+5
+			_mm_store_pd(s_i1j1, sum_1);
+			_mm_store_pd(s_i1j2, sum_2);
+			_mm_store_pd(s_i2j1, sum_3);
+			_mm_store_pd(s_i2j2, sum_4);
 			
 			C[i*lda+j] = ci1j1 + s_i1j1[0] + s_i1j1[1];
 			if (j+1 >= N && i+1 >= M) {
@@ -143,7 +161,6 @@ static void do_block (int lda, int M, int N, int K, double* A, double* B, double
 	free(s_i1j2);
 	free(s_i2j1);
 	free(s_i2j2);
-	free(T);
 }
 
 static void second_block(int lda, int M, int N, int K, double* A, double *B, double *C) {
@@ -157,9 +174,21 @@ static void second_block(int lda, int M, int N, int K, double* A, double *B, dou
 				double *a = A + i*lda + k;
 				double *b = B + k*lda + j;
 				int K_2 = min (BLOCK_1_SIZE, K-k);
+				
+				int K_mem = K_2;
+				if (K_2 % 2 == 1) {
+					K_mem = K_2 + 1;
+				}
 
-				do_block(lda, M_2, N_2, K_2, a, b, c);
+				double* T = NULL;
+			
+				if (posix_memalign(&T, 16, K_mem*N*sizeof(double))) {
+					printf("ERROR ON USING POSIX_MEMALIGN \n");
+					return;	
+				}			
 
+				do_block(lda, M_2, N_2, K_2, a, b, c, T);
+				free(T);
 			}
 		}
 	}
@@ -170,6 +199,8 @@ static void second_block(int lda, int M, int N, int K, double* A, double *B, dou
  * where A, B, and C are lda-by-lda matrices stored in row-major order
  * On exit, A and B maintain their input values. */  
 void square_dgemm (int lda, double* A, double* B, double* C) {
+	
+
 	for (int i = 0; i < lda; i += BLOCK_2_SIZE) {
 		int M = min (BLOCK_2_SIZE, lda - i);
 		for (int j = 0; j < lda; j += BLOCK_2_SIZE) {
